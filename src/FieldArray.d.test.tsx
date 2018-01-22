0eb8bba935865089172bcc0a5560f362dd36ddb3
Kevin Ross <kevin.ross@alienfast.com>
2018-01-22 10:50:58 -0600

    [typescript] add tests and implementation for FieldArrayProps

---
+++ src/FieldArray.d.test.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import { Form, Field } from 'react-final-form'
+import * as arrayMutators from 'final-form-arrays'
+import { FieldArray } from './index'
+
+const onSubmit = async (values: any) => {
+  console.log(values)
+}
+
+const basic = () => (
+  <Form
+    onSubmit={onSubmit}
+    mutators={{
+      ...arrayMutators
+    }}
+  >
+    {({
+      handleSubmit,
+      mutators: { push, pop }, // injected from final-form-arrays above
+      pristine,
+      reset,
+      submitting,
+      values
+    }) => {
+      return (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Company</label>
+            <Field name="company" component="input" />
+          </div>
+          <div className="buttons">
+            <button type="button" onClick={() => push('customers', undefined)}>
+              Add Customer
+            </button>
+            <button type="button" onClick={() => pop('customers')}>
+              Remove Customer
+            </button>
+          </div>
+          <FieldArray name="customers">
+            {({ fields }) =>
+              fields.map((name, index) => (
+                <div key={name}>
+                  <label>Cust. #{index + 1}</label>
+                  <Field
+                    name={`${name}.firstName`}
+                    component="input"
+                    placeholder="First Name"
+                  />
+                  <Field
+                    name={`${name}.lastName`}
+                    component="input"
+                    placeholder="Last Name"
+                  />
+                  <span
+                    onClick={() => fields.remove(index)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    ❌
+                  </span>
+                </div>
+              ))
+            }
+          </FieldArray>
+
+          <div className="buttons">
+            <button type="submit" disabled={submitting || pristine}>
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={reset}
+              disabled={submitting || pristine}
+            >
+              Reset
+            </button>
+          </div>
+          <pre>{JSON.stringify(values)}</pre>
+        </form>
+      )
+    }}
+  </Form>
+)
