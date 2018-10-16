feb5ce33c99dd578799cfe1e61fc0ad42603d099
Aleksey Kolesnikov <askolesnikov70@gmail.com>
2018-10-16 09:38:04 +0700

    [typescript] Add typings for isEqual

--- src/index.d.ts
+++ src/index.d.ts
@@ -45,6 +45,7 @@ export interface RenderableProps<T> {
 export interface FieldArrayProps
   extends RenderableProps<FieldArrayRenderProps> {
   name: string
+  isEqual?: (a: any, b: any) => boolean
   subscription?: FieldSubscription
   validate?: (value: any, allValues: object) => any
 }
